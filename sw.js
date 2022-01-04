3de028bc2f616028be99c75c99ec1a22b8090448
Javier Gutierrez <javier.bz.gt@gmail.com>
2022-01-03 19:18:53 -0600

    cambios

--- sw.js
+++ sw.js
@@ -2,10 +2,12 @@ const nombreCache = "apv-1";
 const archivos = [
 	"/",
 	"/index.html",
+	"/error.html",
 	"/css/bootstrap.css",
 	"/css/styles.css",
 	"/js/app.js",
 	"/js/apv.js",
+	"/manifest.json",
 ];
 
 // Cuando se instala el service worker
@@ -13,8 +15,7 @@ self.addEventListener("install", (e) => {
 	console.log("Instalado el serviceWorker");
 
 	e.waitUntil(
-		caches.open(nombreCache)
-            .then((cache) => {
+		caches.open(nombreCache).then((cache) => {
 			console.log("Cacheando");
 			cache.addAll(archivos);
 		})
@@ -29,8 +30,25 @@ self.addEventListener("activate", (e) => {
 });
 
 // Evento fetch para descargar archivos estáticos
-self.addEventListener("fetch", (e) => {
-	console.log("fetch...");
+// self.addEventListener("fetch", (e) => {
+// 	console.log("fetch...", e);
 
-	console.log(e);
+// 	e.respondWith(
+// 		caches
+// 			.match(e.request)
+// 			.then((respuestaCache) => respuestaCache)
+// 			.catch(() => caches.match("/error.html"))
+// 	);
+// });
+
+self.addEventListener("fetch", (e) => {
+	console.log("Fetch", e);
+
+	e.respondWith(
+		caches
+			.match(e.request)
+			.then((cacheResponse) =>
+				cacheResponse ? cacheResponse : caches.match("error.html")
+			)
+	);
 });
